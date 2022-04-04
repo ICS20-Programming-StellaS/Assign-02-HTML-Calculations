a64850df7c5e62b3c3d9d9ce2232946c5227d8a8
Stella Silvaroli <99274582+Stella-Silvaroli06@users.noreply.github.com>
2022-04-04 14:55:32 +0000

    completed Assign-02-HTML-Calculations

--- js/script.js
+++ js/script.js
@@ -6,19 +6,25 @@
 
 'use strict'
 /**
- * This function calculates area of triangle.
+ * This function calculates area and perimeter of a trapezoid.
  */
 function calculate () {
   // input
   let sideA = parseFloat(document.getElementById('side-a-of-trapezoid').value)
   
   let sideB = parseFloat(document.getElementById('side-b-of-trapezoid').value)
+
+  let sideC = parseFloat(document.getElementById('side-c-of-trapezoid').value)
+  
+  let sideD = parseFloat(document.getElementById('side-d-of-trapezoid').value)
   
   let height = parseFloat(document.getElementById('height-of-trapezoid').value)
   
   // process
   const area = (sideA + sideB) * height/2
+  const perimeter = sideA + sideB + sideC + sideD
   
   // output
   document.getElementById('area').innerHTML = 'The area of the trapezoid is: ' + area.toFixed(2) + ' cm²'
+  document.getElementById('perimeter').innerHTML = 'The perimeter of the trapezoid is: ' + perimeter.toFixed(2) + ' cm²'
 }
